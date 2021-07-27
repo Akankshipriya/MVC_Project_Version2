41807e0615e5df2e7b1f8e970d5db16131ccb7c7
APriya <APriya@BHAVNAWKS651>
2021-07-27 12:14:37 +0530

    Add project files.

---
+++ MVC_Project_Version2/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using MVC_Project_Version2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Project_Version2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            database_testEntities obj = new database_testEntities();
+            return View(obj.displayData());
+        }
+
+        
+    }
+}
---
+++ MVC_Project_Version2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC_Project_Version2.MvcApplication" Language="C#" %>
